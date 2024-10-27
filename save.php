77f17ea5cb552406dafadd6a230016edde936e84
zedhire <my9app.gs@gmail.com>
2024-10-27 20:26:18 +0100

    Delete save.php

--- save.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $text = $_POST['text'] . "\n";
-    file_put_contents('home.csv', $text, FILE_APPEND);
-    echo "تم حفظ النص بنجاح!";
-}
-?>
